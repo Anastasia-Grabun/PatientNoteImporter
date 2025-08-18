ee9298a8a494179fb2103165827cab40e724d33f
Anastasia Grabun <anastasiamoon20@mail.ru>
2025-08-18 11:46:36 +0300

    fix main class

---
+++ src/main/java/org/example/PatientNoteImporterApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientNoteImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientNoteImporterApplication.class, args);
+    }
+
+}
